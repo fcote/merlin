16648aff2f89f321fa8940da3fe66a10460491e8
Fabien COTE <fabien.cote@me.com>
2021-10-16 16:50:27 +0200

    fix(GraphqlContext): add dataloader for subscriptions

--- server/src/resolvers/context.ts
+++ server/src/resolvers/context.ts
@@ -1,5 +1,6 @@
 import { Context } from 'koa'
 
+import { DataloaderService } from '@services/dataloader'
 import { RequestContext } from '@typings/context'
 
 const graphqlContext = ({
@@ -9,11 +10,16 @@ const graphqlContext = ({
   ctx?: Context
   connectionParams?: any
 }): RequestContext => {
+  const isSubscription = !!connectionParams
   const state = ctx?.state ?? connectionParams
 
   const c: RequestContext = {}
   c.user = state.user
   c.userToken = state['x-api-token']
+
+  if (isSubscription) {
+    c.loaders = new DataloaderService(c)
+  }
 
   return c
 }
