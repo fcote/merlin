251dcf6a007d70a4988f5828b1a99532ad22d820
fcote <fabien.cote@me.com>
2022-10-08 17:59:31 +0200

    chore(config): update default cron spec value

--- scheduler/config/config.go
+++ scheduler/config/config.go
@@ -52,7 +52,7 @@ func getBaseConfig() *Config {
 		Timezone: "Europe/Paris",
 		FullSync: CronConfig{
 			Enabled: true,
-			Rule:    "0 0 22 * * 1,2,3,4,5",
+			Rule:    "0 0 0 * * 1,2,3,4,5",
 		},
 		Database: DatabaseConfig{
 			Host:     "localhost:5432",
