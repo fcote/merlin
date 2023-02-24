87fe5b50ebaa41c65303fb4ad307aad913778749
fcote <fabien.cote@me.com>
2023-02-24 23:00:42 +0100

    feat(scheduler): replace sync/atomic by go.uber.org/atomic

--- scheduler/pkg/fmp/rate_limit_timer.go
+++ scheduler/pkg/fmp/rate_limit_timer.go
@@ -1,8 +1,9 @@
 package fmp
 
 import (
-	"sync/atomic"
 	"time"
+
+	"go.uber.org/atomic"
 )
 
 const timerDuration = 1 * time.Minute
@@ -10,15 +11,16 @@ const timerDuration = 1 * time.Minute
 // RateLimitTimer is a timer that keeps track of the number of requests
 type RateLimitTimer struct {
 	timer             *time.Timer
-	end               time.Time
-	nRequests         atomic.Uint64
+	end               *atomic.Time
+	nRequests         *atomic.Uint64
 	maxRequestsPerMin uint64
 }
 
 // NewRateLimitTimer creates a new RateLimitTimer
 func NewRateLimitTimer(maxRequestsPerMin int) *RateLimitTimer {
 	t := &RateLimitTimer{
-		end:               time.Now().Add(timerDuration),
+		end:               atomic.NewTime(time.Now().Add(timerDuration)),
+		nRequests:         atomic.NewUint64(0),
 		maxRequestsPerMin: uint64(maxRequestsPerMin),
 	}
 	t.timer = time.AfterFunc(timerDuration, t.reset)
@@ -31,7 +33,7 @@ func (t *RateLimitTimer) Wait() {
 	t.nRequests.Add(1)
 
 	if t.nRequests.Load() >= t.maxRequestsPerMin {
-		timeToWait := time.Until(t.end)
+		timeToWait := time.Until(t.end.Load())
 		<-time.After(timeToWait)
 	}
 }
@@ -41,5 +43,5 @@ func (t *RateLimitTimer) reset() {
 	t.nRequests.Store(0)
 
 	t.timer.Reset(timerDuration)
-	t.end = time.Now().Add(timerDuration)
+	t.end.Store(time.Now().Add(timerDuration))
 }
